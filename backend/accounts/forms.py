95b492265cf5194fa5e934f84ed17ee1ff6e670b
Mehedi Ashik <mehediashik2001@gmail.com>
2026-07-07 00:53:34 +0600

    Add navbar with Register and Login links

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User, Member
-
+#"Form validation"
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(
